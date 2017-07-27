e1fe193cb68c1032f34fcad1136648a3e32dc1ea
bugfr <bugfroggy@gmail.com>
2017-07-26 20:22:07 -0400

    added https support and created runkey verification

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-var express = require('express');
+var express = require('express'),
+    https = require('https'),
+    bodyParser = require('body-parser');
 var app = express();
 var runkey = require("./hidden/runkey.txt");
 
-app.get('/runkey', function (req, res) {
-    res.send('Hello World!')
+var credentials = { // SSL Credentials
+    ca: fs.readFileSync("../www/certbot/fullchain.pem"),
+    key: fs.readFileSync("../www/certbot/privkey.pem"),
+    cert: fs.readFileSync("../www/certbot/cert.pem")
+};
+
+app.use( bodyParser.json() );       // to support JSON-encoded POST bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded POST bodies
+    extended: true
+}));
+
+app.use(express.json());       // to support JSON-encoded POST bodies
+app.use(express.urlencoded()); // to support URL-encoded POST bodies
+
+
+
+app.get('/', function (req, res) {
+
 });
 
-app.listen(8081, function () {
+app.post('/runkey', function (req, res) {
+    var json = {};
+    // If key is valid
+    if(req.body.key === runkey) {
+        json.ok = true;
+    } else {
+        json.ok = false;
+        json.error = "Invalid key";
+    }
+    res.send(JSON.stringify(json));
+});
+
+var server = https.createServer(credentials, app);
+
+server.listen(8081, function () {
     console.log('Listening');
 });
