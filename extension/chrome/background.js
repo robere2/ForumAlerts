9a8ac023c30cf02cfed68f00a023df6dff93bc02
bugfr <bugfroggy@gmail.com>
2017-07-28 20:38:59 -0400

    started to working querying the forums for new alerts

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,11 +1,39 @@
 var delay = (60 * 1000); // How long between queries to the forums
 var run_key = "wr8yoisfPG0ggb6MSsHYJH3hkMmInkxRTsHjmnNIuv0QjNmGBnnW9igZWuoeYet6"; // Random string that must match on
                                                                                   // https://socket.bugg.co:8880/runkey
+var unreadAlerts, unreadConversations = 0;
 function RunKeyCheckException(error) {
     this.error = error;
 }
 
-setInterval(function() {
+function run() {
+    var runKeyValid = queryRunKey();
+    if(runKeyValid) {
+        queryForum();
+    }
+}
+
+function runKeyCheck(data) {
+    if(data.ok) {
+        console.log("Successfully verified the run key.");
+    } else {
+        throw new RunKeyCheckException(data.error);
+    }
+}
+
+function queryForum() {
+    $.ajax("https://hypixel.net/?_xfResponseType=json", {
+        cache: false
+    }).done(function(data) {
+        if(data._visitor_alertsUnread > unreadAlerts || data._visitor_conversationsUnread > unreadConversations) {
+            newAlert(data._visitor_alertsUnread, data._visitor_conversationsUnread);
+        }
+    }).fail(function() {
+        ajaxFailure("hypixel.net")
+    })
+}
+
+function queryRunKey() {
     $.ajax("https://socket.bugg.co:8081/runkey", {
         cache: false,
         method: "POST",
@@ -16,15 +44,32 @@ setInterval(function() {
         } catch(e) {
             if(e instanceof RunKeyCheckException) {
                 console.error("RunKeyCheckException: " + e.error);
+                return false;
             }
         }
+        return true;
+    }).fail(function() {
+        ajaxFailure("bugg.co")
+        return false;
     });
-}, delay);
-
-function runKeyCheck(data) {
-    if(data.ok) {
-        console.log("Successfully verified the run key.");
-    } else {
-        throw new RunKeyCheckException(data.error);
-    }
 }
+
+
+function ajaxFailure(point) {
+    return chrome.notifications.create(null, {
+        templateType: "basic",
+        iconUrl: "./forums-alert-icon.png",
+        title: "Connection Failure",
+        message: "Failed connecting to " + point + "! Contact bugfroggy if this does not resolve itself."
+    });
+}
+
+function newAlert(alertCount, convoCount) {
+    chrome.notifications.create(null, {
+        templateType: "basic",
+        iconUrl: "./forums-alert-icon.png",
+        title: "New Hypixel Forum Notifications",
+        message: "You have " + alertCount + " unread alert(s) and " + convoCount + " unread conversation(s)."
+    });
+}
+setInterval(run, delay);
