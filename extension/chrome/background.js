4f6c8186cd9ab4c0be2ad7d9f198a475ca4fdb52
bugfr <bugfroggy@gmail.com>
2017-07-29 02:12:18 -0400

    changed delay time back to normal 60s

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,4 +1,4 @@
-var delay = (10 * 1000); // How long between queries to the forums
+var delay = (60 * 1000); // How long between queries to the forums
 var run_key = "wr8yoisfPG0ggb6MSsHYJH3hkMmInkxRTsHjmnNIuv0QjNmGBnnW9igZWuoeYet6"; // Random string that must match on
                                                                                   // https://socket.bugg.co:8880/runkey
 
