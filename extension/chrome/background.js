a7322451a3541e2b78e2a0a80f10cb04dd9c22e2
bugfr <bugfroggy@gmail.com>
2017-08-01 15:21:30 -0400

    copied all changes over to the Chrome version

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -7,6 +7,9 @@ var failures = {"hypixel.net": false, "bugg.co": false}; // Documents whether or
 var unreadAlerts = 0, unreadConversations = 0;
 var maintenance; // Variable storing whether or not the service is currently undergoing maintenance
 
+/**
+ * Initializer; called to start the script
+ */
 function init() {
     chrome.storage.sync.get("maintenance", function(items) {
         maintenance = items.maintenance || false; // Set to default (false) if items.maintenance is falsy (i.e. undefined).
@@ -15,13 +18,21 @@ function init() {
     setInterval(run, delay);
 }
 
+/**
+ * Exception Constructor
+ * @param error Error that occurred.
+ * @constructor
+ */
 function RunKeyCheckException(error) {
     this.error = error;
 }
 
+/**
+ * Runs on an interval determined by delay variable, essentially is the scripts core.
+ */
 function run() {
-    chrome.storage.sync.get("forum_alerts_toggle", function(items) {
-        var runKeyValid = queryRunKey();
+    chrome.storage.sync.get("forum_alerts_toggle", function(items) { // Grabs the settings set by the popup
+        var runKeyValid = queryRunKey(); // Checks whether the local runkey is valid with /runkey.
         if (runKeyValid) {
             if(items.forum_alerts_toggle === "true") {
                 queryForum();
@@ -30,10 +41,16 @@ function run() {
     });
 }
 
+/**
+ * Parses the data returned by /runkey and determines whether the script should continue running.
+ * @param data Data provided by the AJAX call to /runkey.
+ * @returns {boolean} Whether or not the script should continue running.
+ * @throws RunKeyCheckException when data.ok is false for some reason (but not in maintenance).
+ */
 function runKeyCheck(data) {
-    if(data.ok) {
+    if(data.ok) { // If runkey is valid
         console.log("Successfully verified the run key.");
-        maintenanceCheck(data.maintenance);
+        maintenanceCheck(data.maintenance); // Check for changes in maintenance mode
         return true;
     } else if(data.maintenance) {
         maintenanceCheck(data.maintenance);
@@ -43,13 +60,16 @@ function runKeyCheck(data) {
     }
 }
 
+/**
+ * Makes an AJAX call to the forums and then sends a desktop notification if any new alerts/convos have come in.
+ */
 function queryForum() {
     $.ajax("https://hypixel.net/?_xfResponseType=json", {
         cache: false
     }).done(function(data) {
-        reestablish("hypixel.net");
-
         if("_visitor_alertsUnread" in data && "_visitor_conversationsUnread" in data) {
+            reestablish("hypixel.net"); // Connection didn't fail so send re-established notification
+
             var remote_alerts = data._visitor_alertsUnread;
             var remote_convo = data._visitor_conversationsUnread;
             console.log("Unread alerts: " + remote_alerts);
@@ -64,70 +84,85 @@ function queryForum() {
             }
             unreadAlerts = remote_alerts;
             unreadConversations = remote_convo;
-        } else {
+        } else { // Presumably not logged in as no alert or convo data was returned.
             failure("hypixel.net");
         }
-    }).fail(function() {
+    }).fail(function() { // Connection failed
         failure("hypixel.net");
     })
 }
 
+/**
+ * Send an AJAX request to /runkey to determine if the program is authorized to run (implemented in case I ever need to
+ * shut down the program permanently in the future, or support only specific versions)
+ * @returns {boolean} Whether the script is authorized to continue running
+ */
 function queryRunKey() {
-    var return_val = true;
+    var return_val = true; // Value to be returned at the end. Can change throughout the function a bunch so variables.
     $.ajax("https://aws.bugg.co:2083/runkey", {
         cache: false,
         method: "POST",
         data: {key: run_key}
     }).done(function(data) {
-        reestablish("bugg.co");
+        reestablish("bugg.co"); // Connection didn't fail so send re-established notification
 
         try{
-            return_val = runKeyCheck(data);
+            return_val = runKeyCheck(data); // Determine whether /runkey is authorizing the script to proceed
         } catch(e) {
             if(e instanceof RunKeyCheckException) {
                 console.error("RunKeyCheckException: " + e.error);
                 return_val = false;
-            } else {
+            } else { // Technically not uncaught but whatever
                 console.error("Uncaught Exception: " + e.toString());
                 return_val = false;
             }
         }
-    }).fail(function() {
-        if(!maintenance) {
-            failure("bugg.co");
-        }
+    }).fail(function() { // Connection failed
+        failure("bugg.co");
         return_val = false;
     });
     return return_val;
 }
 
-
+/**
+ * Called whenever there is some sort of connection error or failure to gather data
+ * @param point The host that the failure is occurring on.
+ */
 function failure(point) {
     console.error("Failed to connect to " + point);
 
-    if(!failures[point]) {
-        failures[point] = true;
+    if(!maintenance) { // If maintenance mode is enabled, then it is assumed that this is known and isn't a need to panic.
 
-        return chrome.notifications.create(null, {
-            type: "basic",
-            iconUrl: "./pics/forum-alerts-64x.png",
-            title: "Connection Failure",
-            message: "Failed connecting to " + point + "! Contact bugfroggy if this does not resolve itself. (Are you logged in?)"
-        }, function(id) {
-            addNotification(id, "error");
-            console.log("Error Notification ID: " + id);
-        });
+        if (!failures[point]) { // If a notification for this request hasn't already been sent out
+            failures[point] = true;
+
+            return chrome.notifications.create(null, {
+                type: "basic",
+                iconUrl: "./pics/forum-alerts-64x.png",
+                title: "Connection Failure",
+                message: "Failed connecting to " + point + "! Contact bugfroggy if this does not resolve itself. (Are you logged in?)"
+            }, function (id) {
+                addNotification(id, "error");
+                console.log("Error Notification ID: " + id);
+            });
+        }
     }
 }
 
+/**
+ * Checks whether or not a maintenance notification needs to be sent out/updated.
+ * @param status Maintenance status returned from /runkey.
+ */
 function maintenanceCheck(status) {
 
-    if(status !== maintenance) {
+    if(status !== maintenance) { // If the status of maintenance mode has changed
         chrome.storage.sync.set({maintenance: status}, function() {
             console.log("Updated maintenance status");
         });
         maintenance = status;
+
         var title, message;
+        // Determine the message to send out with the notification
         if(maintenance) {
             title = "Undergoing Maintenance";
             message = "This service is currently unavailable due to maintenance. You will be notified when it is back online.";
@@ -143,14 +178,18 @@ function maintenanceCheck(status) {
             message: message
         }, function(id) {
             addNotification(id, "error");
-            console.log("Error Notification ID: " + id);
+            console.log("Maintenance Notification ID: " + id);
         });
     }
 }
 
+/**
+ * Called whenever a connection issue that resulted in the call of failure() is fixed.
+ * @param point The host that the failure was occurring on.
+ */
 function reestablish(point) {
 
-    if(failures[point]) {
+    if(failures[point]) { // If a request to this host has failed before now
         console.error("Reconnected to " + point);
         failures[point] = false;
 
@@ -158,7 +197,7 @@ function reestablish(point) {
             type: "basic",
             iconUrl: "./pics/forum-alerts-64x.png",
             title: "Connection Reestablished",
-            message: "Connection to " + point + " reestablished."
+            message: "Connection to " + point + " reestablished. Sorry about the inconvenience."
         }, function(id) {
             addNotification(id, "error");
             console.log("Error Notification ID: " + id);
@@ -166,6 +205,11 @@ function reestablish(point) {
     }
 }
 
+/**
+ * Creates a new desktop notification for a new forum alert or convo
+ * @param alertCount Number of alerts on the forum
+ * @param convoCount Number of unread convos on the forum
+ */
 function newAlert(alertCount, convoCount) {
     chrome.notifications.create(null, {
         type: "basic",
@@ -178,6 +222,9 @@ function newAlert(alertCount, convoCount) {
     });
 }
 
+/**
+ * Opens the appropriate tab upon notifications being clicked and then closes the notification
+ */
 chrome.notifications.onClicked.addListener(function(id) {
     console.log("Clicked");
     if($.inArray(id, notifications.alert > -1)) {
@@ -188,7 +235,9 @@ chrome.notifications.onClicked.addListener(function(id) {
     chrome.notifications.clear(id);
 });
 
-// Check installed status
+/**
+ * Checks for the extension's install status, and if first install, sets the default settings.
+ */
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason === "install"){
         chrome.storage.sync.set({'forum_alerts_toggle': "true"}, function() {
@@ -197,6 +246,11 @@ chrome.runtime.onInstalled.addListener(function(details){
     }
 });
 
+/**
+ * Stores the notification ID in an array and associates it with what type of notification it is.
+ * @param id The notification ID
+ * @param type The type this notification is
+ */
 function addNotification(id, type) {
     notifications[type].push(id);
 }
